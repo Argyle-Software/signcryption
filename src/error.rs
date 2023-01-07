820c9a39615f57b56bf8907aae9a47c83f03bdca
Mitchell Berry <foss@mitchellberry.com>
2023-01-07 22:37:11 +1100

    Add: docs

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,18 @@
+
+/// Signcryption failure modes
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SignCryptError {
+  /// Additional Authenticated Data provided to [`crate::sign_before`] and [`crate::verify_before`] 
+  /// functions is larger than the allowed maximum of 255 bytes.
   InvalidLength,
-  Generic,
+  /// The signature was unable to be verified after decryption
   Mismatch,
+  /// Error occurred during the encryption phase of [`crate::signcrypt`]
   Encryption,
+  /// Error occurred during decryption phase of [`crate::unsigncrypt`]
   Decryption,
-  NonCanonicalSignature
+  /// The signature is not canonical
+  NonCanonicalSig,
+  /// Catch-all for other errors
+  Generic,
 }

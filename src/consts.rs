321688662483858ff255684dd956410f8b99e63e
Mitchell Berry <foss@mitchellberry.com>
2023-01-08 12:01:32 +1100

    Modify visibility

--- src/consts.rs
+++ src/consts.rs
@@ -1,11 +1,22 @@
+/// Length of the public key
 pub const PUBLICKEYBYTES: usize = 32;
+
+/// Length of the secret key
 pub const SECRETKEYBYTES: usize = 32;
+
+/// Length of the generated shared secret 
 pub const SHAREDBYTES: usize = 32;
+
+/// Length of the seed for keypair creation 
 pub const SEEDBYTES: usize = 64;
+
+/// Length of the resulting signature
 pub const SIGNBYTES: usize = 64;
-pub const NONCEBYTES: usize = 24;
+
+/// Tag length
 pub const MACBYTES: usize = 16;
 
+pub(crate) const NONCEBYTES: usize = 12;
 pub(crate) const SCALARBYTES: usize = 32;
 pub(crate) const CHALLENGEBYTES: usize = 32;
 pub(crate) const BYTES: usize = 32;
